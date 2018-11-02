a32a4036fe4e8ac8fbd4afef833baedbcc6765c8
hs-glenn-lee <git_test@mail.com>
2018-11-02 18:50:16 +0900

    changin ui

--- src/components/global-header.vue
+++ src/components/global-header.vue
@@ -1,14 +1,27 @@
 <template>
   <header class="global-header user-select-none">
-    <div class="flex-con-row"><!-- if not signed-in -->
-      <div class="flex-item item-mg-8">
-        <div @click="showMenu" class="menu-icon psd-hover-cursor-pointer">GOTO</div>
-      </div>
-      <div class="flex-item">
-        <div class="logo">logo</div>
-      </div>
-      <div class="flex-con">
-        <div @click="goToSignIn" class="header-link item-mg-8 psd-hover-cursor-pointer">로그인(로그인후 사람모양 아이콘)</div>
+    <div class="center-block h-inh">
+      <div class="flex-con-row h-inh"><!-- if not signed-in -->
+        <div class="flex-item flex-con-row">
+          <div class="logo flex-item">logo</div>
+          <div class="flex-item flex-con-row">
+            <span class="header-nav-rep">소개</span>
+            <span class="">
+              <span class="header-nav-rep-nav bold-on-hover">함께하기</span>
+              <nav class="sub">
+                <ul>
+                  <li>다운로드</li>
+                  <li>고객센터</li>
+                </ul>
+              </nav>
+            </span>
+            <span class="header-nav-rep">요금제</span>
+          </div>
+        </div>
+        <div class="flex-con-row h-inh">
+          <div @click="goToSignIn" class="header-nav flex-item item-mg-8 psd-hover-cursor-pointer">로그인</div>
+          <div @click="showMenu" class="menu-icon flex-item psd-hover-cursor-pointer">GOTO</div>
+        </div>
       </div>
     </div>
     <menu-of-global-header ref="$menuOfGlobalHeader"></menu-of-global-header>
@@ -39,6 +52,7 @@ export default {
 <style scoped>
   header {
     border-bottom: 1px solid #e5e5e5;
+    height: 70px;
   }
 
   .flex-con-row {
@@ -48,23 +62,32 @@ export default {
     justify-content: space-between;
   }
   .flex-item {
-    display:inline-block;
+    display: flex;
   }
   .item-mg-8 {
     margin: 8px;
   }
   .logo {
     /* temp */
-    display: inline-block;
-
     background-color: #c3c3c3;
     text-align: center;
     width:280px;
     height: 32px;
   }
 
-  .header-link {
-    padding: 4px;
-    margin: 0px 4px 0px 4px;
+  .h-inh {
+    height: inherit;
   }
+  .header-nav-rep {
+    font-size: 1.5em;
+    color: #6d6f7b;
+    padding: 4px;
+    margin: 0px 8px 0px 8px;
+  }
+
+  .bold-on-hover:hover {
+    color: #323648;
+    font-weight: bold;
+  }
+
 </style>
